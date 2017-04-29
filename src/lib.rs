c525f24bcaab5614136ae9bbfa5ef9488c1c5a85
Michael Hewson <michael@michaelhewson.ca>
2017-04-29 14:07:17 -0400

    Fix declare_stackptr! and add some use cases
    - support `let mut` as well as `let`
    -  Improved documentation. Doc tests aren’t passing though, haven’t
    figured that out yet

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,18 @@
 //! Provides `StackPtr`, an owned pointer to stack-allocated data. This is useful for casting a value to an unsized type (e.g. trait object) while maintaining ownership, without doing a heap allocation with `Box`.
+//!
+//! # Usage
+//!
+//! ```
+//! #[macro_use]
+//! extern crate stack_ptr;
+//! use stack_ptr::StackPtr;
+//!
+//! fn main() {
+//!     declare_stackptr! {
+//!         let f: StackPtr<[_]> = StackPtr::new([1,2,3]);
+//!     }
+//! }
+//! ```
 #![cfg_attr(feature = "nightly", feature(unsize, coerce_unsized))]
 use std::marker::PhantomData;
 use std::{ptr, mem};
@@ -69,25 +83,76 @@ pub fn lifetime_of<'a, T>(_ref: &'a mut T) -> PhantomData<&'a mut ()> {
     PhantomData
 }
 
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __declare_stackptr_variable {
+    (no, $name:ident, $expr:expr) => {
+        let $name = $expr;
+    };
+    (yes, $name:ident, $expr:expr) => {
+        let mut $name = $expr;
+    };
+}
+
+#[doc(hidden)]
+#[macro_export]
+macro_rules! __declare_stackptr {
+    ($mutable:ident, $name:ident, $ty:ty, $expr:expr) => {
+        let mut _value = $expr;
+        let mut _lifetime_marker = ();
+        __declare_stackptr_variable!($mutable, $name, unsafe {
+            let ptr = &mut _value as *mut $ty;
+            ::std::mem::forget(_value);
+            StackPtr::from_raw_parts(ptr, $crate::lifetime_of(&mut _lifetime_marker))
+        });
+    };
+}
+
 /// Safely declare a `StackPtr` with the appropriate lifetime at this point on the stack.
 ///
 /// ```
+/// # #[test]
+/// # fn test_stackptr_macro {
+/// let v = vec![1,2,3];
 /// declare_stackptr! {
-///     let foo: StackPtr<> = StackPtr::new();
+///     let closure = StackPtr::new(|| {
+///         for x in vec.iter() {
+///             // do something with x
+///         }
+///     });
 /// }
+/// # }
 /// ```
 #[macro_export]
-        let mut _value = $expr;
-        let mut _lifetime_marker = ();
-            let ptr = &mut _value as *mut $ty;
-            ::std::mem::forget(_value);
-            StackPtr::new(ptr, &mut _lifetime_marker)
-        };
-    };
 macro_rules! declare_stackptr {
+    (let $name:ident: StackPtr<$ty:ty> = StackPtr::new($expr:expr);) => {
+        __declare_stackptr!(no, $name, $ty, $expr)
+    };
+    (let $name:ident = StackPtr::new($expr:expr);) => {
+        __declare_stackptr!(no, $name, _, $expr)
+    };
+    (let mut $name:ident: StackPtr<$ty:ty> = StackPtr::new($expr:expr);) => {
+        __declare_stackptr!(yes, $name, $ty, $expr)
+    };
+    (let mut $name:ident = StackPtr::new($expr:expr);) => {
+        __declare_stackptr!(yes, $name, _, $expr)
+    };
 }
 
 /// An implementation of `std::ops::CoerceUnsized` on stable rust. On nightly, you can convert a `StackPtr<T>` into a `StackPtr<U>` if `T` implements `U`, with `let sp = sp as StackPtr<U>;`, but this requires the unstable `CoerceUnsized` trait. On stable you can do `let sp = coerce_stackptr!(sp, U);`.
+///
+/// # Example:
+///
+/// ```
+/// fn append_closure<'vec, 'f, F>(closures: &'vec mut Vec<StackPtr<'vec, FnOnce()>>, f: StackPtr<'f, F>)
+/// where F: FnOnce() {
+///     #[cfg(feature = "nightly")]
+///     let f = f as StackPtr<FnOnce()>;
+///     #[cfg(not(feature = "nightly"))]
+///     let f = coerce_stackptr!(f, FnOnce());
+///     closures.push(f);
+/// }
+/// ```
 #[macro_export]
 macro_rules! coerce_stackptr {
     ($sp:expr, $ty:ty) => {{
@@ -105,4 +170,17 @@ mod nightly {
     use std::marker::Unsize;
 
     impl<'a, T, U> CoerceUnsized<StackPtr<'a, U>> for StackPtr<'a, T> where T: Unsize<U> + ?Sized, U: ?Sized {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::StackPtr;
+    #[test]
+    fn test_basic() {
+        declare_stackptr!{
+            let slice: StackPtr<[i32]> = StackPtr::new([1,2,3,4,5]);
+        }
+
+        assert_eq!(&*slice, &[1,2,3,4,5]);
+    }
 }
